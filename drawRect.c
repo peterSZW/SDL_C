d999017cea8dda3cbf5d33c6bb801678e0e10bb3
steven <steven@stevendeMac-mini.local>
2020-09-26 22:11:35 +0800

    add mac modify

--- drawRect.c
+++ drawRect.c
@@ -15,9 +15,7 @@ int main(int argc, char *argv[])
     render = SDL_CreateRenderer(window, -1, 0);
  
     SDL_RenderClear(render);
-    SDL_SetRenderDrawColor(render, 200, 200, 100, 255);
-    SDL_RenderDrawLine(render, 100, 420, 400, 420);
-        
+           
 	int x=0;
 
     while (1)
@@ -26,23 +24,48 @@ int main(int argc, char *argv[])
         if (event.type == SDL_QUIT)
             break;
 
-        SDL_SetRenderDrawColor(render, 0, 0, 0, 0);
-        SDL_RenderClear(render);
-        SDL_SetRenderDrawColor(render, 200, 200, 100, 255);
-        SDL_RenderDrawLine(render, 100, 400, 400, 400);
-		SDL_RenderDrawLine(render, 100, 420, 400, 420);
-        rect2.x=x;
-        SDL_RenderDrawRect(render, &rect1);
-        SDL_RenderFillRect(render, &rect1);
+        if(event.type==SDL_KEYDOWN)
+        {
+            //通过不同的键标检测来选择表面
+            switch(event.key.keysym.sym)
+            {
+            case SDLK_UP:
+                rect1.y-=1;
+                break;
+            case SDLK_DOWN:
+                rect1.y+=1;
+                break;
+            case SDLK_LEFT:
+                rect1.x--;
+                break;
+            case SDLK_RIGHT:
+                rect1.x++;
+                
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(render, 0, 0, 0, 0); //设置笔的颜色为 0，0，0
+        SDL_RenderClear(render); //清除屏幕
+
+        SDL_SetRenderDrawColor(render, 200, 200, x, 255); //设置笔的颜色为 200，100，255
+        SDL_RenderDrawLine(render, 100, 400, 400, 400); //画一条线（100，400），（400，400）
+		SDL_RenderDrawLine(render, 100, 420, 400, 420); //画一条线（100，420），（400，420）
+
+        SDL_SetRenderDrawColor(render, x, 200, 200, 255); //设置笔的颜色为 200，100，255
+        SDL_RenderDrawRect(render, &rect1); // 画一方格
+        SDL_RenderFillRect(render, &rect1); //方格涂颜色
      
-        SDL_SetRenderDrawColor(render, 200, 200, 200, 255);
-        SDL_RenderDrawRect(render, &rect2);
-        SDL_RenderFillRect(render, &rect2);
-        SDL_RenderPresent(render);
+        SDL_SetRenderDrawColor(render, 200,x, 200, 255);//设置笔的颜色为 0，0，0
+        rect2.x=x; //第二个方格的 x 坐标 等于不断增加的  x 值
+        SDL_RenderDrawRect(render, &rect2); // 画一方格
+        SDL_RenderFillRect(render, &rect2); //方格涂颜色
         
-        SDL_Delay(1);
-        x=x+1;
-        if (x>600) x=0;
+        SDL_RenderPresent(render); //显示画布
+        
+        SDL_Delay(1); //延迟1ms
+        x=x+5;
+        if (x>600) x=0; // 如果方格的x坐标超出屏幕，那么设置方格的x坐标为 0 
     }
 
     SDL_DestroyRenderer(render);
--- mac_gcc.sh
+++ mac_gcc.sh
@@ -1,1 +1,1 @@
-gcc drawRect.c -I/usr/local/Cellar/sdl2/2.0.12_1/include/SDL2 -L/usr/local/Cellar/sdl2/2.0.12_1/lib -lSDL2
+gcc drawRect.c -I/usr/local/Cellar/sdl2/2.0.12_1/include/SDL2 -L/usr/local/Cellar/sdl2/2.0.12_1/lib -lSDL2 && ./a.out
